20bf89e4410f31bbf214897ff928e66d3e236a34
Aawaiz Ali <114678240+AawaizAli@users.noreply.github.com>
2024-10-05 13:34:39 +0500

    added testing jsons

--- petproj/app/api/pets/route.js
+++ petproj/app/api/pets/route.js
@@ -86,7 +86,7 @@ export async function PUT(req) {
     const client = createClient();
 
     try {
-        await client.connect(); // Connect to the database
+        await client.connect(); 
         const result = await client.query(
             'UPDATE pets SET owner_id = $1, pet_name = $2, pet_type = $3, pet_breed = $4, city_id = $5, area = $6, age = $7, description = $8, adoption_status = $9, adoption_price = $10, min_age_of_children = $11, can_live_with_dogs = $12, can_live_with_cats = $13, must_have_someone_home = $14, energy_level = $15, cuddliness_level = $16, health_issues = $17 WHERE pet_id = $18 RETURNING *',
             [owner_id, pet_name, pet_type, pet_breed, city_id, area, age, description, adoption_status, adoption_price, min_age_of_children, can_live_with_dogs, can_live_with_cats, must_have_someone_home, energy_level, cuddliness_level, health_issues, pet_id]
@@ -110,16 +110,16 @@ export async function PUT(req) {
             headers: { 'Content-Type': 'application/json' },
         });
     } finally {
-        await client.end(); // Always close the connection
+        await client.end(); 
     }
 }
 
 export async function DELETE(req) {
-    const { pet_id } = await req.json(); // Assuming you're sending the pet_id in the body of the DELETE request
+    const { pet_id } = await req.json();
     const client = createClient();
 
     try {
-        await client.connect(); // Connect to the database
+        await client.connect(); 
         const result = await client.query('DELETE FROM pets WHERE pet_id = $1 RETURNING *', [pet_id]);
 
         if (result.rows.length === 0) {
@@ -140,6 +140,6 @@ export async function DELETE(req) {
             headers: { 'Content-Type': 'application/json' },
         });
     } finally {
-        await client.end(); // Always close the connection
+        await client.end();
     }
 }
