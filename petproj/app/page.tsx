d18d4db718e64960a4a1e9a0172f6fa3600238c2
Aawaiz <aawaizali8@gmail.com>
2024-11-20 23:45:46 +0500

    front page change

--- petproj/app/page.tsx
+++ petproj/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from "react";
 import Navbar from "@/components/navbar";
+import Image from "next/image";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState<boolean | null>(null);
@@ -31,22 +32,8 @@ export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center p-24" style={{ backgroundColor: 'rgb(var(--background-color))' }}>
-
+        <Image src="/maroonLogo.png" alt="Logo" className="mx-auto" width={250} height={100}/>
         {/* Connection Status Indicator */}
-        <div className="mt-4 flex justify-center p-4" style={{ color: 'rgb(var(--background-color))' }}>
-          {loading ? (
-            <span>Checking database connection...</span>
-          ) : isConnected ? (
-            <span >Database Connected!</span>
-          ) : (
-            <span >Database Not Connected!</span>
-          )}
-        </div>
-
-        <div className="mt-32">
-          By Talha Idrees, Sohail Ahmed and Zain
-        </div>
-
         <div className="mt-2">
           Coming Soon!
         </div>
