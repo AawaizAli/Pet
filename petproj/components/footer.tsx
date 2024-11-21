e14b21b842f500f3c8d0ce246ac6eaef16d7f5fe
Aawaiz <aawaizali8@gmail.com>
2024-11-22 04:29:06 +0500

    4prod

--- petproj/components/footer.tsx
+++ petproj/components/footer.tsx
@@ -3,29 +3,29 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
-
 const Footer = () => {
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-
   const hideFooterRoutes = ["/login", "/register"];
-  const pathName=usePathname();
-  const isHideFooter = hideFooterRoutes.includes(pathName);
-  
-  if(isHideFooter)
-  {
-    return null;
-  }
-  
+  const pathName = usePathname();
 
+  // Update the year unconditionally
   useEffect(() => {
     setCurrentYear(new Date().getFullYear());
   }, []);
 
+  // Determine if the footer should be hidden
+  const isHideFooter = hideFooterRoutes.includes(pathName);
+
+  // Conditional rendering for the footer
+  if (isHideFooter) {
+    return null;
+  }
+
   return (
-    <footer className="text-white p-6 rounded-t-[3rem] rounded-b-none" style={{backgroundColor:'#A03048'}}>
+    <footer className="text-white p-6 rounded-t-[3rem] rounded-b-none" style={{ backgroundColor: '#A03048' }}>
       <div className="container mx-auto text-center">
         <div className="mb-4">
-          <Image src="/paltu_logo.svg" alt="Logo" className="mx-auto" width={250} height={100}/>
+          <Image src="/paltu_logo.svg" alt="Logo" className="mx-auto" width={250} height={100} />
         </div>
         <div className="mb-4">
           <p>Follow us on Instagram</p>
@@ -34,7 +34,7 @@ const Footer = () => {
           </a>
         </div>
         <div className="mb-4">
-        { /* for about us */}
+          { /* for about us */ }
         </div>
         <p className="text-sm">&copy; {currentYear} Paltuu. All rights reserved.</p>
       </div>
