000775976a44ab1228e94858bb0eff24d37093cd
Aawaiz <aawaizali8@gmail.com>
2024-11-25 23:27:42 +0500

    footer

--- petproj/app/layout.tsx
+++ petproj/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import ClientProvider from "./ClientProvider"; // Import your ClientProvider
 import "./globals.css";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 const montserrat = Montserrat({
     subsets: ["latin"],
