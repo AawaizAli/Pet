aa6cdf70ddc9d63d074b5767c347d69a2c366538
shuja-16 <smshuja.16@gmail.com>
2024-12-09 08:40:27 +0500

    ijjoij

--- petproj/app/vet-get-verified-1/page.tsx
+++ petproj/app/vet-get-verified-1/page.tsx
@@ -48,13 +48,9 @@ const VerificationInfo = () => {
                         onClick={handleStartVerification}
                         className="bg-primary text-white rounded-3xl px-4 py-2 flex items-center gap-2">
                         <span>Start Verification Process</span>
-                        <Image
-                            src="/arrow-right.svg"
-                            className="arrow"
-                            alt="Arrow pointing right"
-                            width={16}
-                            height={16}
-                        />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right text-white" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+</svg>
                     </button>
                 </div>
             </div>
