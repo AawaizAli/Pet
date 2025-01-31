62a786695e30dbcd1ef9a24cc00550917b4535e5
Aawaiz <aawaizali8@gmail.com>
2025-01-31 22:55:33 +0500

    add +92 in number on signup

--- petproj/app/sign-up/page.tsx
+++ petproj/app/sign-up/page.tsx
@@ -111,14 +111,26 @@ const CreateUser = () => {
                         <label className="block text-gray-700 text-sm font-medium mb-1">
                             Phone Number
                         </label>
-                        <input
-                            type="text"
-                            value={phone_number}
-                            onChange={(e) => setPhoneNumber(e.target.value)}
-                            className="w-full border border-gray-300 rounded-xl px-3 py-2 focus:ring-2 focus:ring-primary focus:outline-none"
-                            required
-                        />
+                        <div className="flex space-x-2">
+                            {/* Disabled input for country code */}
+                            <input
+                                type="text"
+                                value="+92"
+                                className="w-12 border border-gray-300 pl-2 rounded-xl py-2 focus:ring-2 focus:ring-primary focus:outline-none"
+                                disabled
+                            />
+                            {/* Input for remaining phone number */}
+                            <input
+                                type="text"
+                                value={phone_number}
+                                onChange={(e) => setPhoneNumber(e.target.value)}
+                                placeholder="3338888666"
+                                className="w-full border border-gray-300 rounded-xl px-3 py-2 focus:ring-2 focus:ring-primary focus:outline-none"
+                                required
+                            />
+                        </div>
                     </div>
+
 
                     {/* Password */}
                     <div className="relative">
