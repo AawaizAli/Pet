76cc80f4d216d80e9f7e6283738a5c55f4d348f6
Aawaiz <aawaizali8@gmail.com>
2025-03-15 03:05:55 +0500

    color change

--- petproj/components/petGrid.tsx
+++ petproj/components/petGrid.tsx
@@ -64,10 +64,28 @@ const PetGrid: React.FC<PetGridProps> = ({ pets }) => {
                 Create new listing
             </Link>
 
-            <Link
+            {/* <Link
                 href="/create-listing"
                 className="fixed bottom-4 right-2 sm:hidden z-50">
                 <button className="flex items-center gap-2 bg-primary text-white p-3 rounded-2xl shadow-lg hover:bg-primary-dark transition-all duration-300 hover:scale-105">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="h-4 w-4"
+                        viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+                    </svg>
+                    <span className="text-sm">Create new Listing</span>
+                </button>
+            </Link> */}
+
+            <Link
+                href="/create-listing"
+                className="fixed bottom-4 right-2 sm:hidden z-50">
+                <button className="flex items-center gap-2 bg-white text-primary border-2 border-primary p-3 rounded-2xl shadow-lg hover:bg-primary-dark transition-all duration-300 hover:scale-105">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="16"
